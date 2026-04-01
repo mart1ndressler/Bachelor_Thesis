71818358f3603836d79826741c526ea4ae8c21bb
Martin Dressler <m.dressler321@gmail.com>
2026-04-01 02:22:25 +0200

    Delete DRE0065_BP directory

--- DRE0065_BP/js/app/init_app.js
+++
@@ -1,6 +0,0 @@
-function initAppStartup(){
-  const savedLanguage = (typeof getLang === 'function') ? getLang() : (localStorage.getItem('language') || 'en');
-  changeLanguage(savedLanguage);
-}
-
-document.addEventListener('DOMContentLoaded', initAppStartup);
